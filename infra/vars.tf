3e0b212df818b9f05b6d0a72665fc2df829c47fe
purna <purnaraomba@gmail.com>
2024-01-24 20:47:16 +0530

    Roboshop Update

--- infra/vars.tf
+++ infra/vars.tf
@@ -5,43 +5,7 @@ variable "instances" {
       type = "t2.micro"
     }
     user = {
-      name = "mongodb"
-      type = "t2.micro"
-    }
-    user = {
-      name = "catalogue"
-      type = "t2.micro"
-    }
-    user = {
-      name = "redis"
-      type = "t2.micro"
-    }
-    user = {
-      name = "user"
-      type = "t2.micro"
-    }
-    user = {
-      name = "cart"
-      type = "t2.micro"
-    }
-    user = {
-      name = "mysql"
-      type = "t2.micro"
-    }
-    user = {
-      name = "shipping"
-      type = "t2.micro"
-    }
-    user = {
-      name = "rabbitmq"
-      type = "t2.micro"
-    }
-    user = {
-      name = "payment"
-      type = "t2.micro"
-    }
-    user = {
-      name = "dispatch"
+      name = "useer"
       type = "t2.micro"
     }
   }

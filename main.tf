67e95c31caca27ca76379d8524d4984fedb726d4
purna <purnaraomba@gmail.com>
2024-01-25 19:33:45 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "ec2" {
   for_each      = var.instances
-  source        = "ec2"
+  source        = "./ec2"
   component     = each.value["name"]
   instance_type = each.value["type"]
   sg_id         = module.sg.sg_id

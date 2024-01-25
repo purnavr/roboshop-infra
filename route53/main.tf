4832cb797710772d8cc3aac3c3498df1db29f004
purna <purnaraomba@gmail.com>
2024-01-25 20:27:50 +0530

    Roboshop Update

--- route53/main.tf
+++ route53/main.tf
@@ -1,9 +1,10 @@
-resource "aws_route53_record" "record" {
+resource "aws_route53_record" "records" {
+  zone_id = "Z01280802SKTCPOFIGWX3"
   name    = "${var.component}-roboz.online"
   type    = "A"
   ttl     = 30
   records = [var.private_ip]
-  zone_id = "Z01280802SKTCPOFIGWX3"
+
 }
 
 variable "component" {}

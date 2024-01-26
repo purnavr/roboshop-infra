82dbc3f2d3aa5b910be8de71788a606647c758e0
purna <purnaraomba@gmail.com>
2024-01-26 19:20:15 +0530

    Roboshop Update

--- ec22/main.tf
+++ ec22/main.tf
@@ -1,5 +1,5 @@
 resource "aws_spot_instance_request" "ec2" {
-  ami                    = "ami-03265a0778a880afb"
+  ami                    = "ami-0f3c7d07486cad139"
   instance_type          = "t3.nano"
 
   tags = {

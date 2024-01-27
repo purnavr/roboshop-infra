f9976c2d607f7d4688c90d37e3881c80ca7974b0
purna <purnaraomba@gmail.com>
2024-01-27 10:34:08 +0530

    Roboshop Update

---
+++ ec/main.tf
@@ -0,0 +1,33 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+resource "aws_spot_instance_request" "ec2" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t2.micro"
+
+  tags = {
+    Name = "ec2"
+  }
+}
+
+#data "aws_ami" "ami" {
+#  most_recent = true
+#  name_regex  = "Centos-8-DevOps-Practice"
+#  owners      = ["973714476881"]
+#}
+#
+#
+#resource "aws_spot_instance_request" "ec2" {
+#  ami                    = data.aws_ami.ami.id
+#  instance_type          = t2.micro
+#  vpc_security_group_ids = [var.sg_id]
+#  wait_for_fulfillment   = true
+#
+#  tags = {
+#    Name = var.component
+#  }
+#}

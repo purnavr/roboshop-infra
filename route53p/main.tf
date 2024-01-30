7f86a667917c4b21a002279310d970d42ecae0c8
purna <purnaraomba@gmail.com>
2024-01-30 19:11:57 +0530

    Roboshop Update

--- route53p/main.tf
+++ route53p/main.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "publicc" {
   zone_id = "Z01280802SKTCPOFIGWX3"
-  name    = "roboz.online"
+  name    = "frontend.online"
   type    = "A"
   ttl     = 30
   records = ["39.230.9.2"]

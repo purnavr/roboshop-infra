bd1835e9114235262c611c54ffda7f6fe11357ac
purna <purnaraomba@gmail.com>
2024-01-30 19:38:02 +0530

    Roboshop Update

--- route53/main.tf
+++ route53/main.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "records" {
   zone_id = "Z01280802SKTCPOFIGWX3"
-  name    = "${var.component}-roboz.online"
+  name    = "${var.component}.roboz.online"
   type    = "A"
   ttl     = 30
   records = [var.private_ip]
--- route53p/main.tf
+++ route53p/main.tf
@@ -1,15 +1,7 @@
-resource "aws_route53_record" "publicc" {
-  zone_id = "Z01280802SKTCPOFIGWX3"
-  name    = "frontend.online"
-  type    = "A"
-  ttl     = 30
-  records = ["52.230.9.2"]
-}
-
-resource "aws_route53_record" "public" {
-  zone_id = "Z01280802SKTCPOFIGWX3"
-  name    = "roboz.online"
-  type    = "A"
-  ttl     = 30
-  records = ["41.173.18.73"]
-}
+#resource "aws_route53_record" "publicc" {
+#  zone_id = "Z01280802SKTCPOFIGWX3"
+#  name    = "frontend.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = ["52.230.9.2"]
+#}

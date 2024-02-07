aa28d951e17f2f6ec0d4d6cc1ff59dcff05ec12a
purna <purnaraomba@gmail.com>
2024-02-07 14:58:59 +0530

    Roboshop Update

--- aws-parameters/main.tf
+++ aws-parameters/main.tf
@@ -1,8 +1,8 @@
 resource "aws_ssm_parameter" "par" {
   for_each = var.parameters
-  name = each.value[name]
-  type = each.value[type]
-  value = each.value[value]
+  name = each.value["name"]
+  type = each.value["type"]
+  value = each.value["value"]
 }
 
 variable "parameters" {}

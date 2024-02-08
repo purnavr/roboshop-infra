d5ebc262c9cf0e1882a08b1bd3b49f4b75d260f7
purna <purnaraomba@gmail.com>
2024-02-08 18:37:18 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "ec2" {
   component     = each.value["name"]
   instance_type = each.value["type"]
   sg_id         = module.sg.sg_id
-#  private_ip = each.value["name"].privateip
+  private_ip = each.value["name"].[privateip]
 }
 
 module "sg" {

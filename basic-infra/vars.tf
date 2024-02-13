b92485e7dd965c72169ddd6ebd2d96dc4400d5ad
purna <purnaraomba@gmail.com>
2024-02-13 15:58:39 +0530

    Roboshop Update

--- basic-infra/vars.tf
+++ basic-infra/vars.tf
@@ -2,47 +2,47 @@ variable "instances" {
   default = {
     frontend = {
       name = "frontend"
-      type = "t3.micro"
+      type = "t2.micro"
     }
     mongodb = {
       name = "mongodb"
-      type = "t3.micro"
+      type = "t2.micro"
     }
     catalogue = {
       name = "catalogue"
-      type = "t3.micro"
+      type = "t2.micro"
     }
     redis = {
       name = "redis"
-      type = "t3.micro"
+      type = "t2.micro"
     }
     user = {
       name = "user"
-      type = "t3.micro"
+      type = "t2.micro"
     }
     cart = {
       name = "cart"
-      type = "t3.micro"
+      type = "t2.micro"
     }
     mysql = {
       name = "mysql"
-      type = "t3.micro"
+      type = "t2.micro"
     }
     shipping = {
       name = "shipping"
-      type = "t3.micro"
+      type = "t2.micro"
     }
     rabbitmq = {
       name = "rabbitmq"
-      type = "t3.micro"
+      type = "t2.micro"
     }
     payment = {
       name = "payment"
-      type = "t3.micro"
+      type = "t2.micro"
     }
     dispatch = {
       name = "dispatch"
-      type = "t3.micro"
+      type = "t2.micro"
     }
   }
 }

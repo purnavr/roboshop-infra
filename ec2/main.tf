4e66c66fd5d68e936526d01499f3e26b8cdf8d60
purna <purnaraomba@gmail.com>
2024-02-15 19:05:06 +0530

    Roboshop Update

--- ec2/main.tf
+++ ec2/main.tf
@@ -73,7 +73,7 @@ resource "aws_iam_policy" "ssm-policy" {
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource": "arn:aws:ssm:us-east-1:860050401100:parameter/${var.env}-${var.component}*"
+        "Resource": "arn:aws:ssm:us-east-1:860050401100:parameter/${var.env}.${var.component}*"
       },
       {
         "Sid": "VisualEditor1",

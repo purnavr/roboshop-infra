fcb84b5fd0d1f207c35b64ccba142a9e4057a6f0
purna <purnaraomba@gmail.com>
2024-02-16 21:50:58 +0530

    Roboshop Update

--- aws-parameters/main.tf
+++ aws-parameters/main.tf
@@ -9,9 +9,9 @@ variable "parameters" {}
 
 resource "aws_ssm_parameter" "secrets" {
   count = length(var.secrets)
-  name = var.parameters[count.index].name
-  type = var.parameters[count.index].type
-  value = var.parameters[count.index].value
+  name = var.secrets[count.index].name
+  type = var.secrets[count.index].type
+  value = var.secrets[count.index].value
 }
 
 variable "secrets" {}

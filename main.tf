f83746d2d35f9bfa8434d9215cb0f5e6cd8c4920
purna <purnaraomba@gmail.com>
2024-03-22 19:40:04 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 module "vpc" {
-  source        = "git::https://github.com/purnavr/tf-module-vpc.git"
-  for_each      = var.vpc
-  vpc_cidr      = each.value["vpc_cidr"]
-  env           = var.env
+  source = "git::https://github.com/purnavr/tf-module-vpc.git"
+  env = var.env
+
+  for_each = var.vpc
+  vpc_cidr = each.value["vpc_cidr"]
+
 }
 
 

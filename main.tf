0bd15c5fe40f0122b4489029201b2cb6c73fe6ed
purna <purnaraomba@gmail.com>
2024-03-28 20:22:36 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -19,21 +19,21 @@ output "vpc" {
 #  value = local.db_subnet_ids
 #}
 
-module "docdb" {
-  source = "git::https://github.com/purnavr/tf-module-docdb.git"
-  env = var.env
-  tags = var.tags
-  for_each = var.docdb
-  engine = each.value["engine"]
-  engine_version = each.value["engine_version"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot = each.value["skip_final_snapshot"]
-  no_of_instances = each.value["no_of_instances"]
-  instance_class = each.value["instance_class"]
-  #subnet_ids = local.db_subnet_ids
-
-}
+#module "docdb" {
+#  source = "git::https://github.com/purnavr/tf-module-docdb.git"
+#  env = var.env
+#  tags = var.tags
+#  for_each = var.docdb
+#  engine = each.value["engine"]
+#  engine_version = each.value["engine_version"]
+#  backup_retention_period = each.value["backup_retention_period"]
+#  preferred_backup_window = each.value["preferred_backup_window"]
+#  skip_final_snapshot = each.value["skip_final_snapshot"]
+#  no_of_instances = each.value["no_of_instances"]
+#  instance_class = each.value["instance_class"]
+#  #subnet_ids = local.db_subnet_ids
+#
+#}
 
 #module "rds" {
 #  source = "git::https://github.com/purnavr/tf-module-rds.git"

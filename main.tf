30303304543fafc2b6d2544d21a61264d430a588
purna <purnaraomba@gmail.com>
2024-04-02 18:04:43 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -11,72 +11,72 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
 }
 
-output "vpc" {
-  value = module.vpc
-}
-
 #output "vpc" {
-#  value = local.db_subnet_ids
+#  value = module.vpc
+#}
+
+output "vpc" {
+  value = local.db_subnet_ids
+}
+#
+#module "docdb" {
+#  source = "git::https://github.com/purnavr/tf-module-docdb.git"
+#  env = var.env
+#  tags = var.tags
+#  for_each = var.docdb
+#  engine = each.value["engine"]
+#  engine_version = each.value["engine_version"]
+#  backup_retention_period = each.value["backup_retention_period"]
+#  preferred_backup_window = each.value["preferred_backup_window"]
+#  skip_final_snapshot = each.value["skip_final_snapshot"]
+#  no_of_instances = each.value["no_of_instances"]
+#  instance_class = each.value["instance_class"]
+#  subnet_ids = local.db_subnet_ids
+#
 #}
 #
-module "docdb" {
-  source = "git::https://github.com/purnavr/tf-module-docdb.git"
-  env = var.env
-  tags = var.tags
-  for_each = var.docdb
-  engine = each.value["engine"]
-  engine_version = each.value["engine_version"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot = each.value["skip_final_snapshot"]
-  no_of_instances = each.value["no_of_instances"]
-  instance_class = each.value["instance_class"]
-  subnet_ids = local.db_subnet_ids
-
-}
-
-module "rds" {
-  source = "git::https://github.com/purnavr/tf-module-rds.git"
-  env = var.env
-  tags = var.tags
-
-  subnet_ids = local.db_subnet_ids
-
-  for_each = var.rds
-  engine = each.value["engine"]
-  engine_version = each.value["engine_version"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  no_of_instances = each.value["no_of_instances"]
-  instance_class = each.value["instance_class"]
-}
-
-module "elasticache" {
-  source = "git::https://github.com/purnavr/tf-module-elasticache.git"
-  env = var.env
-  tags = var.tags
-
-  subnet_ids = local.db_subnet_ids
-
-  for_each = var.elasticache
-  engine = each.value["engine"]
-  node_type = each.value["node_type"]
-  num_cache_nodes = each.value["num_cache_nodes"]
-  engine_version = each.value["engine_version"]
-  port = each.value["port"]
-}
-
-module "rabbitmq" {
-  source = "git::https://github.com/purnavr/tf-module-rabbitmq.git"
-  env = var.env
-  tags = var.tags
-
-  subnet_ids = local.db_subnet_ids
-
-  for_each = var.rabbitmq
-  instance_type = each.value["instance_type"]
-
-}
+#module "rds" {
+#  source = "git::https://github.com/purnavr/tf-module-rds.git"
+#  env = var.env
+#  tags = var.tags
+#
+#  subnet_ids = local.db_subnet_ids
+#
+#  for_each = var.rds
+#  engine = each.value["engine"]
+#  engine_version = each.value["engine_version"]
+#  backup_retention_period = each.value["backup_retention_period"]
+#  preferred_backup_window = each.value["preferred_backup_window"]
+#  no_of_instances = each.value["no_of_instances"]
+#  instance_class = each.value["instance_class"]
+#}
+#
+#module "elasticache" {
+#  source = "git::https://github.com/purnavr/tf-module-elasticache.git"
+#  env = var.env
+#  tags = var.tags
+#
+#  subnet_ids = local.db_subnet_ids
+#
+#  for_each = var.elasticache
+#  engine = each.value["engine"]
+#  node_type = each.value["node_type"]
+#  num_cache_nodes = each.value["num_cache_nodes"]
+#  engine_version = each.value["engine_version"]
+#  port = each.value["port"]
+#}
+#
+#module "rabbitmq" {
+#  source = "git::https://github.com/purnavr/tf-module-rabbitmq.git"
+#  env = var.env
+#  tags = var.tags
+#
+#  subnet_ids = local.db_subnet_ids
+#
+#  for_each = var.rabbitmq
+#  instance_type = each.value["instance_type"]
+#
+#}
 
 #module "alb" {
 #  source = "git::https://github.com/purnavr/tf-module-alb.git"

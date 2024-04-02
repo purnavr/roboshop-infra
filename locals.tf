40f138cb4277f320d8975f5f3576f25ec8131420
purna <purnaraomba@gmail.com>
2024-04-02 15:22:57 +0530

    Roboshop Update

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
     db = tolist([module.vpc["main"].private_subnets["db-az1"].id, module.vpc["main"].private_subnets["db-az2"].id])
     app = tolist([module.vpc["main"].private_subnets["app-az1"].id, module.vpc["main"].private_subnets["app-az2"].id])
     web = tolist([module.vpc["main"].private_subnets["web-az1"].id, module.vpc["main"].private_subnets["web-az2"].id])
-    public = tolist([module.vpc["main"].public_subnets["public-az1"].id, module.vpc["main"].private_subnets["public-az2"].id])
+    #public = tolist([module.vpc["main"].public_subnets["public-az1"].id, module.vpc["main"].private_subnets["public-az2"].id])
   }
 #  subnet_cidr ={
 #    db = tolist([module.vpc["main"].private_subnets["db-az1"].cidr_block, module.vpc["main"].private_subnets["db-az2"].cidr_block])

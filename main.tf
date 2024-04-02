cb0d5279546b81f190afcec97543b8bcb9caa107
purna <purnaraomba@gmail.com>
2024-04-02 20:06:32 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -91,22 +91,22 @@ module "alb" {
 
 }
 
-#module "app" {
-#  source = "git::https://github.com/purnavr/tf-module-app.git"
-#  env = var.env
-#  tags = var.tags
-#
-#  vpc_id = module.vpc["main"].vpc_id
-#  bastion_cidr = var.bastion_cidr
-#
-#  for_each = var.apps
-#  component = each.value["component"]
-#  instance_type = each.value["instance_type"]
-#  desired_capacity = each.value["desired_capacity"]
-#  max_size = each.value["max_size"]
-#  min_size = each.value["min_size"]
-#  subnets = lookup(local.subnet_ids, each.value["subnet_name"], null )
-#  port = each.value["port"]
-#  allow_app_to = lookup(local.subnet_cidr, each.value["allow_app_to"], null )
-#
-#}
+module "app" {
+  source = "git::https://github.com/purnavr/tf-module-app.git"
+  env = var.env
+  tags = var.tags
+
+  vpc_id = module.vpc["main"].vpc_id
+  bastion_cidr = var.bastion_cidr
+
+  for_each = var.apps
+  component = each.value["component"]
+  instance_type = each.value["instance_type"]
+  desired_capacity = each.value["desired_capacity"]
+  max_size = each.value["max_size"]
+  min_size = each.value["min_size"]
+  subnets = lookup(local.subnet_ids, each.value["subnet_name"], null )
+  port = each.value["port"]
+  allow_app_to = lookup(local.subnet_cidr, each.value["allow_app_to"], null )
+
+}

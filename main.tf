63d78893f7690c49b56f8810e148c021fb3407b4
purna <purnaraomba@gmail.com>
2024-04-03 19:57:15 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -15,9 +15,9 @@ output "vpc" {
   value = module.vpc
 }
 
-#output "alb" {
-#  value = module.alb
-#}
+output "alb" {
+  value = module.alb
+}
 
 #output "vpc" {
 #  value = local.db_subnet_ids
@@ -82,41 +82,41 @@ output "vpc" {
 #
 #}
 
-#module "alb" {
-#  source = "git::https://github.com/purnavr/tf-module-alb.git"
-#  env = var.env
-#  tags = var.tags
-#
-#  vpc_id = module.vpc["main"].vpc_id
-#
-#  for_each = var.alb
-#  name = each.value["name"]
-#  internal = each.value["internal"]
-#  load_balancer_type = each.value["load_balancer_type"]
-#  subnets = lookup(local.subnet_ids, each.value["subnet_name"], null)
-#  allow_cidr = each.value["allow_cidr"]
-#}
-#
-#module "app" {
-#  source = "git::https://github.com/purnavr/tf-module-app.git"
-#  env = var.env
-#  tags = var.tags
-#
-#  vpc_id = module.vpc["main"].vpc_id
-#
-#  bastion_cidr = var.bastion_cidr
-#  dns_domain = var.dns_domain
-#
-#  for_each = var.apps
-#  component = each.value["component"]
-#  instance_type = each.value["instance_type"]
-#  desired_capacity = each.value["desired_capacity"]
-#  max_size = each.value["max_size"]
-#  min_size = each.value["min_size"]
-#  subnets = lookup(local.subnet_ids, each.value["subnet_name"], null )
-#  port = each.value["port"]
-#  allow_app_to = lookup(local.subnet_cidr, each.value["allow_app_to"], null )
-#  alb_dns_name = lookup(lookup(lookup(module.alb, each.value["alb"], null), "alb", null) "dns_name", null)
-#  listener_arn = lookup(lookup(lookup(module.alb, each.value["alb"], null), "listener", null) "arn", null)
-#  listener_priority = each.value["listener_priority"]
-#}
+module "alb" {
+  source = "git::https://github.com/purnavr/tf-module-alb.git"
+  env = var.env
+  tags = var.tags
+
+  vpc_id = module.vpc["main"].vpc_id
+
+  for_each = var.alb
+  name = each.value["name"]
+  internal = each.value["internal"]
+  load_balancer_type = each.value["load_balancer_type"]
+  subnets = lookup(local.subnet_ids, each.value["subnet_name"], null)
+  allow_cidr = each.value["allow_cidr"]
+}
+
+module "app" {
+  source = "git::https://github.com/purnavr/tf-module-app.git"
+  env = var.env
+  tags = var.tags
+
+  vpc_id = module.vpc["main"].vpc_id
+
+  bastion_cidr = var.bastion_cidr
+  dns_domain = var.dns_domain
+
+  for_each = var.apps
+  component = each.value["component"]
+  instance_type = each.value["instance_type"]
+  desired_capacity = each.value["desired_capacity"]
+  max_size = each.value["max_size"]
+  min_size = each.value["min_size"]
+  subnets = lookup(local.subnet_ids, each.value["subnet_name"], null )
+  port = each.value["port"]
+  allow_app_to = lookup(local.subnet_cidr, each.value["allow_app_to"], null )
+  alb_dns_name = lookup(lookup(lookup(module.alb, each.value["alb"], null), "alb", null) "dns_name", null)
+  listener_arn = lookup(lookup(lookup(module.alb, each.value["alb"], null), "listener", null) "arn", null)
+  listener_priority = each.value["listener_priority"]
+}

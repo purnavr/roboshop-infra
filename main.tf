6d264ada6059df5646bfeca9e70f5618355b5b12
purna <purnaraomba@gmail.com>
2024-07-03 19:15:17 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -136,7 +136,7 @@ module "app" {
   parameters = each.value["parameters"]
 }
 
-/*
+
 ### Load Runner
 resource "aws_spot_instance_request" "load-runner" {
   ami           = data.aws_ami.ami.id
@@ -176,5 +176,5 @@ resource "null_resource" "load-gen" {
     ]
   }
 }
-*/
+
 

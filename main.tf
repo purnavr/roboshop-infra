77ad1083a2af3cb23795b9acebe4697932939772
purna <purnaraomba@gmail.com>
2024-07-22 20:51:12 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
 }
 
+/*
 output "vpc" {
   value = module.vpc
 }
@@ -178,4 +179,35 @@ resource "null_resource" "load-gen" {
     ]
   }
 }
+*/
 
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region    = "us-east-1"
+  cluster_name  = "minikube"
+  aws_instance_type = "t3.medium"
+  ssh_public_key = "~/.ssh/id_rsa.pub"
+  aws_subnet_id = lookup(local.subnet_ids,"public", null)[0]
+  hosted_zone = "roboz.online"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
+
+output "MINIKUBE_SERVER" {
+  value = "ssh centos@${module.minikube.public_ip}"
+}
+
+output "MINIKUBE_CONFIG" {
+  value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/ .kube/config"
+}

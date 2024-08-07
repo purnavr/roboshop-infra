2c282eefc9243642e64fc2bff32d56f8842f5145
purna <purnaraomba@gmail.com>
2024-08-07 12:59:02 +0530

    Roboshop Update

--- data.tf
+++ data.tf
@@ -1,8 +1,8 @@
-#data "aws_ami" "ami" {
-#  most_recent = true
-#  name_regex  = "centos-8-ansible-image"
-#  owners      = ["self"]
-#}
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "centos-8-ansible-image"
+  owners      = ["self"]
+}
 
 data "aws_ssm_parameter" "ssh_pass" {
   name = "${var.env}.ssh.pass"
